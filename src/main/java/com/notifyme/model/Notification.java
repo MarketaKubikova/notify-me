4b887d1df105c43ea410ea84e0106c3319432a39
Markéta Kubíková <kubikovamarket@gmail.com>
2025-03-13 21:42:31 +0100

    added core entities

---
+++ src/main/java/com/notifyme/model/Notification.java
@@ -0,0 +1,21 @@
+package com.notifyme.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String topic;
+    private String message;
+    private boolean delivered;
+    @ManyToOne
+    private User user;
+}
---
+++ src/main/java/com/notifyme/model/User.java
@@ -0,0 +1,22 @@
+package com.notifyme.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    @ElementCollection
+    private Set<String> subscribedTopics = new HashSet<>();
+}
